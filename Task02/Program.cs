62cd709bca366520782cf1c307c55f7adda2ec17
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-14 06:30:35 -0800

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1: Напишите программу, 
+// которая принимает на вход три числа и проверяет,
+//  может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона 
+// треугольника меньше суммы двух других сторон.
+
+int Prompt(string message)
+{
+    Console.WriteLine($"{message} ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Triangle(int A, int B, int C)
+{
+    if (A + B > C)
+    {
+       return true;
+    }
+    return false;
+}
+int A = Prompt("Enter side A");
+int B = Prompt("Enter side B");
+int C = Prompt("Enter side C");
+if (Triangle(A, B, C) && Triangle(B, C, A) && Triangle(A, C, B))
+{
+    
+    Console.WriteLine("Triangle may be with this sides ");
+}
+else
+{
+Console.WriteLine("No, it isn't really ");
+}
